e5d0db23cf479be2c1d62e1315526e79732e8477
Crazycoder-python <jaswitamedipudi@gmail.com>
2021-08-03 11:55:00 -0500

    A_Little_Dictonary.py

---
+++ A_Little_Dictonary.py
@@ -0,0 +1,268 @@
+name = input('What is your name? ==>:')
+print('hello {0} welcome to a little dictonary'.format(name))
+intro = input('pick a word to find its meaning\nCwtch\nEuouae\nHonorificabilitudinitatibus\nRotavator\n'
+              'Anagnorisis\nAbstentious\nAesthetical\nIngenious\nTantalizing\nIncandescent\n Enter Here ==>:')
+inputi = 'Do you want to continue?==>:'
+#  The while loop
+running = True
+while running:
+    #  The definitions with the key words
+    if intro == 'Cwtch':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print(
+            'Cwtch, which has long been a familiar word in the Welsh language, was given two definitions: noun (Welsh) '
+            '1. a cupboard or cubbyhole. 2. a cuddle or hug.\n ... This corresponds with the word\'s other meaning, which is\n'
+            ' a place to safely store things – if you give someone a cwtch,\n you\'re figuratively giving them a \'safe place\'.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+    elif intro == 'Euouae':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Euouae or Evovae is an abbreviation used in Latin \npsalters and other liturgical books to show the '
+              'distribution of syllables in the differentia or variable\n melodic endings of the standard Psalm tones of '
+              'Gregorian chant.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'euouae':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Euouae or Evovae is an abbreviation used in Latin \npsalters and other liturgical books to show the '
+              'distribution of syllables in the differentia or variable\n melodic endings of the standard Psalm tones of '
+              'Gregorian chant.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'Honorificabilitudinitatibus':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Honorificabilitudinitatibus is the dative and ablative \nplural of the '
+              'medieval Latin word honōrificābilitūdini'
+              'tās, which can be translated as "the state of being \nable to achieve honours". '
+              'It is mentioned by the character'
+              ' Costard in Act V, Scene I of William Shakespeare\'s \nLove\'s Labour\'s Lost.')
+        if input(inputi) == 'Yes'.casefold():
+            print('ok')
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'honorificabilitudinitatibus':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Honorificabilitudinitatibus is the dative and ablative \nplural of the '
+              'medieval Latin word honōrificābilitūdini'
+              'tās, which can be translated as "the state of being \nable to achieve honours". '
+              'It is mentioned by the character'
+              ' Costard in Act V, Scene I of William Shakespeare\'s \nLove\'s Labour\'s Lost.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+
+    elif intro == 'Rotavator':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Rotovator (ˈrəʊtəˌveɪtə ) trademark.\n a type of machine with rotating blades that break up soil.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'rotavator':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Rotovator (ˈrəʊtəˌveɪtə ) trademark.\n a type of machine with rotating blades that break up soil.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'Anagnorisis':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('the point in a play, novel, etc., in which a principal\n character recognizes or d'
+              'iscovers another character\'s'
+              ' \ntrue identity or the true nature of their own circumstances.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'anagnorisis':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('the point in a play, novel, etc., in which a principal\n character recognizes or d'
+              'iscovers another character\'s'
+              ' \ntrue identity or the true nature of their own circumstances.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'Abstentious':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print(' self-restraining; not indulging an appetite especially for\n food or'
+              ' drink. synonyms: abstinent abstemious.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'abstentious':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print(' self-restraining; not indulging an appetite especially for\n food or'
+              ' drink. synonyms: abstinent abstemious.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'Aesthetical':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('concerning or characterized by an appreciation of beauty or good taste.\n synonyms: aesthetic,'
+              ' esthetic, esthet'
+              'ical artistic.\n satisfying aesthetic standards and sensibilities.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'aesthetical':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('concerning or characterized by an appreciation of beauty or good taste.\n synonyms: aesthetic,'
+              ' esthetic, esthet'
+              'ical artistic.\n satisfying aesthetic standards and sensibilities.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'Ingenious':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Ingenious means “characterized by cleverness” or “cleverly inventive,\n” as in contriving new expla'
+              'nations or '
+              'methods:\n an ingenious device; ingenious designers.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'ingenious':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('Ingenious means “characterized by cleverness” or “cleverly inventive,\n” as in contriving new expla'
+              'nations or '
+              'methods:\n an ingenious device; ingenious designers.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+    elif intro == 'Tantalizing':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('possessing a quality that arouses or stimulates desire or \ninterest also : mocking'
+              'ly or teasingly out of reach.\n'
+              ' Other Words from tantalizing Example Sentences.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'tantalizing':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('possessing a quality that arouses or stimulates desire or \ninterest also : mocking'
+              'ly or teasingly out of reach.\n'
+              ' Other Words from tantalizing Example Sentences.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'Incandescent':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('While the word "incandescent" is one way to talk about the electric lamps in your living room,\n it can '
+              'also be used in reference to the brightness\n or someone\'s intellect or personality.')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'incandescent':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('While the word "incandescent" is one way to talk about the electric lamps in your living room,\n it can '
+              'also be used in reference to the brightness\n or someone\'s intellect or personality')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+    elif intro == 'Attachment':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('1: The act of joining things \n'
+              '2: A Strong feeling of connection or affection\n'
+              '3: A device that connects to something\n'
+              '4: Something added to a document of a computer')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+    elif intro == 'attachment':
+        print(
+            '—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('1: The act of joining things \n'
+              '2: A Strong feeling of connection or affection\n'
+              '3: A device that connects to something\n'
+              '4: Something added to a document of a computer')
+        if input(inputi) == 'Yes'.casefold():
+            continue
+        else:
+            print('You dum')
+            running = False
+
+
+    else:
+        print('—————————————————————————————————————————————————————————————————————————————————————————————————————————')
+        print('ERROR WRONG INPUT PLEASE CHECK!!!')
+        running = False
+print("Thank you for using the dictionary, "
+      "next time YOU need to"
+      " pay money if you want to use the dictionary\n, "
+      "But for now Its all on the house! :) ")
